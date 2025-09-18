eb3ddb968cc8cca942e52ff60e819fe154e317db
Sanjeev <rauniyarsanjeev742@gmail.com>
2025-09-18 19:47:12 +0545

    Slider task completed

--- react-intern/src/App.jsx
+++ react-intern/src/App.jsx
@@ -1,37 +1,17 @@
-// import { useState } from 'react'
-// import './App.css'
-// import HelloWorld from './HelloWorld'
-// import Counter from './counter.jsx'
-
-// function App() {
-
-//   return (
-//     <div className="App">
-//       {<HelloWorld />}
-//       {<Counter />}
-//     </div>
-//   )
-// }
-
-// export default App
-
-
+import { useState } from 'react'
 import './App.css'
-import Button from './Button'
-import Input from './Input'
+import HelloWorld from './HelloWorld'
+import Counter from './counter.jsx'
 
 function App() {
+
   return (
     <div className="App">
-       {/* Inputs */}
-      <Input label="Username" placeholder="Enter your username" />
-      <Input placeholder="Only placeholder" />
-
-      {/* Buttons */}
-      <Button buttonText="Submit" variant="primary" />
-      <Button buttonText="Cancel" variant="secondary" />
+      {<HelloWorld />}
+      {<Counter />}
     </div>
   )
 }
 
 export default App
+
